35f7f62e07ad6b91c178adbec054bdb00c3c61d8
Renato Marciano <renato_vtr@hotmail.com>
2022-05-26 23:06:23 -0300

    HerancaPoo/

--- HerancaPoo/src/main/kotlin/Main.kt
+++ HerancaPoo/src/main/kotlin/Main.kt
@@ -4,6 +4,7 @@ import heranca.Preguica
 
 fun main(args: Array<String>) {
     /*
+    
     Crie uma hierarquia de classes conforme abaixo com os seguintes atributos e comportamentos (observe a tabela),
     utilize os seus conhecimentos e distribua as características de forma que tudo o que for comum a todos os animais
     fique na classe Animal:
@@ -18,6 +19,7 @@ fun main(args: Array<String>) {
 
     Implemente um programa que crie os 3 tipos de animais definidos no exercício anterior e invoque o método que
     emite o som de cada um de forma polimórfica, isto é, independente do tipo de animal.
+
      */
 
     var cachorro = Cachorro(nome = "Papa", idade = 20)
@@ -27,7 +29,7 @@ fun main(args: Array<String>) {
     cachorro.emitirSon()
     cavalo.emitirSon()
     preguica.emitirSon()
-println("\n")
+    println("\n")
     cachorro.correr()
     cavalo.correr()
     preguica.correr()
